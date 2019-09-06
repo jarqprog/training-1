1e8fe56d70ec607731c91c3ccd5d12e2c9f819fd
Anna Grochowina <annagrochowina@MacBook-Air-Anna.local>
2019-09-06 20:54:21 +0200

    Initial commit

---
+++ src/main/java/com/ania/training/Bicycle.java
@@ -0,0 +1,19 @@
+package com.ania.training;
+
+public class Bicycle {
+
+    private final int wheels;
+    public int getWheels() {
+        return wheels;
+    }
+
+    public Bicycle() {
+        this.wheels = 2;
+    }
+
+    public Bicycle(int wheels) {
+        this.wheels = wheels;
+    }
+
+
+}
